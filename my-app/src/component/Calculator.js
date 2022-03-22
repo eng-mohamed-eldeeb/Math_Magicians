c4c8a95ad917c86555d397b2b072173a912f7aea
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-03-23 00:53:54 +0200

    Fix the screenshot

--- my-app/src/component/Calculator.js
+++ my-app/src/component/Calculator.js
@@ -1,84 +1,44 @@
 import React from 'react';
 
-
-class Element extends React.Component {
+class Element extends React.PureComponent {
   render() {
-    return(
-    <div className="constaner">
-      <div className="answer"><h1>0</h1></div>
-    <div className="btnWrapper">
-      <div className="a-row">
-        <button type="button" className="btn">Ac</button>
-        <button type="button" className="btn">+/-</button>
-        <button type="button" className="btn">%</button>
-        <button type="button" className="btn o">÷</button>
+    return (
+      <div className="constaner">
+        <div className="answer"><h1>0</h1></div>
+        <div className="btnWrapper">
+          <div className="a-row">
+            <button type="button" className="btn">Ac</button>
+            <button type="button" className="btn">+/-</button>
+            <button type="button" className="btn">%</button>
+            <button type="button" className="btn o">÷</button>
+          </div>
+          <div className="a-row">
+            <button type="button" className="btn">7</button>
+            <button type="button" className="btn">8</button>
+            <button type="button" className="btn">9</button>
+            <button type="button" className="btn o">x</button>
+          </div>
+          <div className="a-row">
+            <button type="button" className="btn">4</button>
+            <button type="button" className="btn">5</button>
+            <button type="button" className="btn">6</button>
+            <button type="button" className="btn o">-</button>
+          </div>
+          <div className="a-row">
+            <button type="button" className="btn">1</button>
+            <button type="button" className="btn">2</button>
+            <button type="button" className="btn">3</button>
+            <button type="button" className="btn o">+</button>
+          </div>
+          <div className="a-row">
+            <button type="button" className="btn zero">0</button>
+            <button type="button" className="btn">.</button>
+            <button type="button" className="btn o">=</button>
+          </div>
+        </div>
       </div>
-      <div className="a-row">
-        <button type="button" className="btn">7</button>
-        <button type="button" className="btn">8</button>
-        <button type="button" className="btn">9</button>
-        <button type="button" className="btn o">x</button>
-      </div>
-      <div className="a-row">
-        <button type="button" className="btn">4</button>
-        <button type="button" className="btn">5</button>
-        <button type="button" className="btn">6</button>
-        <button type="button" className="btn o">-</button>
-      </div>
-      <div className="a-row">
-        <button type="button" className="btn">1</button>
-        <button type="button" className="btn">2</button>
-        <button type="button" className="btn">3</button>
-        <button type="button" className="btn o">+</button>
-      </div>
-      <div className="a-row">
-        <button type="button" className="btn zero">0</button>
-        <button type="button" className="btn">.</button>
-        <button type="button" className="btn o">=</button>
-      </div>
-      </div>
-    </div>
-    )
-  };
+    );
+  }
 }
-// function Element() {
-//   return (
-//     <div className="constaner">
-//       <div className="answer"><h1>0</h1></div>
-//     <div className="btnWrapper">
-//       <div className="a-row">
-//         <button type="button" className="btn">Ac</button>
-//         <button type="button" className="btn"></button>
-//         <button type="button" className="btn"></button>
-//         <button type="button" className="btn o"></button>
-//       </div>
-//       <div className="a-row">
-//         <button type="button" className="btn">{fer}</button>
-//         <button type="button" className="btn">{se}</button>
-//         <button type="button" className="btn">{th}</button>
-//         <button type="button" className="btn o">{forth}</button>
-//       </div>
-//       <div className="a-row">
-//         <button type="button" className="btn">{fer}</button>
-//         <button type="button" className="btn">{se}</button>
-//         <button type="button" className="btn">{th}</button>
-//         <button type="button" className="btn o">{forth}</button>
-//       </div>
-//       <div className="a-row">
-//         <button type="button" className="btn">{fer}</button>
-//         <button type="button" className="btn">{se}</button>
-//         <button type="button" className="btn">{th}</button>
-//         <button type="button" className="btn o">{forth}</button>
-//       </div>
-//       <div className="a-row">
-//         <button type="button" className="btn">{fer}</button>
-//         <button type="button" className="btn">{se}</button>
-//         <button type="button" className="btn">{th}</button>
-//         <button type="button" className="btn o">{forth}</button>
-//       </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default Element;
