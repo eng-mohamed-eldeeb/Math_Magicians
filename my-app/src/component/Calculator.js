19bd53f4019aedbe5e222587334e94bae426ea73
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-03-24 17:34:40 +0200

    Change the class component to a function based

--- my-app/src/component/Calculator.js
+++ my-app/src/component/Calculator.js
@@ -1,104 +1,95 @@
 import React from 'react';
 import calculate from '../logic/calculate';
 
-class Element extends React.PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+function Element() {
+  const [state, setState] = React.useState({});
+
+  function ClickHandler(e) {
+    const result = calculate(state, e.target.textContent);
+    setState(result);
   }
 
-  ClickHandler = (e) => {
-    const result = calculate(this.state, e.target.textContent);
-    this.setState(result);
-  };
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="constaner">
-        <div className="answer">
-          <h1>{[total, operation, next]}</h1>
+  const { total, next, operation } = state;
+  return (
+    <div className="constaner">
+      <div className="answer">
+        <h1>{[total, operation, next]}</h1>
+      </div>
+      <div className="btnWrapper">
+        <div className="a-row">
+          <button type="button" className="btn" onClick={ClickHandler}>
+            AC
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            +/-
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            %
+          </button>
+          <button type="button" className="btn o" onClick={ClickHandler}>
+            ÷
+          </button>
         </div>
-        <div className="btnWrapper">
-          <div className="a-row">
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              AC
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              +/-
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              %
-            </button>
-            <button type="button" className="btn o" onClick={this.ClickHandler}>
-              ÷
-            </button>
-          </div>
-          <div className="a-row">
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              7
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              8
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              9
-            </button>
-            <button type="button" className="btn o" onClick={this.ClickHandler}>
-              x
-            </button>
-          </div>
-          <div className="a-row">
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              4
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              5
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              6
-            </button>
-            <button type="button" className="btn o" onClick={this.ClickHandler}>
-              -
-            </button>
-          </div>
-          <div className="a-row">
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              1
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              2
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              3
-            </button>
-            <button type="button" className="btn o" onClick={this.ClickHandler}>
-              +
-            </button>
-          </div>
-          <div className="a-row">
-            <button
-              type="button"
-              className="btn zero"
-              onClick={this.ClickHandler}
-            >
-              0
-            </button>
-            <button type="button" className="btn" onClick={this.ClickHandler}>
-              .
-            </button>
-            <button type="button" className="btn o" onClick={this.ClickHandler}>
-              =
-            </button>
-          </div>
+        <div className="a-row">
+          <button type="button" className="btn" onClick={ClickHandler}>
+            7
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            8
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            9
+          </button>
+          <button type="button" className="btn o" onClick={ClickHandler}>
+            x
+          </button>
+        </div>
+        <div className="a-row">
+          <button type="button" className="btn" onClick={ClickHandler}>
+            4
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            5
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            6
+          </button>
+          <button type="button" className="btn o" onClick={ClickHandler}>
+            -
+          </button>
+        </div>
+        <div className="a-row">
+          <button type="button" className="btn" onClick={ClickHandler}>
+            1
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            2
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            3
+          </button>
+          <button type="button" className="btn o" onClick={ClickHandler}>
+            +
+          </button>
+        </div>
+        <div className="a-row">
+          <button
+            type="button"
+            className="btn zero"
+            onClick={ClickHandler}
+          >
+            0
+          </button>
+          <button type="button" className="btn" onClick={ClickHandler}>
+            .
+          </button>
+          <button type="button" className="btn o" onClick={ClickHandler}>
+            =
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Element;
